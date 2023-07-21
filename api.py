9cf43f3d1bfcb0778bc2624211c85a4765587617
Ramon <ramon.schmocker@hispeed.ch>
2023-07-21 18:23:12 +0200

    dunno maybe

--- api.py
+++ api.py
@@ -9,15 +9,20 @@ def check_api_availability(api_list_file):
         api_url = api_endpoint.strip()
         try:
             response = requests.get(api_url)
-            if response.status_code == 200: 
+            if response.status_code == 200:
                 CHECK_NODE.append(api_url)
         except requests.exceptions.RequestException:
-            pass  
+            pass
 
     return CHECK_NODE
 
 if __name__ == "__main__":
     api_list_file = "api.txt"
     CHECK_NODE = check_api_availability(api_list_file)
-    print("Available APIs:")
-    print(CHECK_NODE)
+    
+    if CHECK_NODE:
+        chosen_api = CHECK_NODE[0]
+        print("Chosen API endpoint:", chosen_api)
+        
+    else:
+        print("No available APIs found.")
